56be25cd3b118f8904835b5f4e0e9c43385c1d31
Alyssia Bunts <alyssiabunts@gmail.com>
2023-07-01 22:49:42 -0400

    Removed pandas library

--- sloppy_joes_bingo_generator.py
+++ sloppy_joes_bingo_generator.py
@@ -1,4 +1,4 @@
-import random, pandas
+import random
 
 occurrence = ["Kid out too late", "Wheely bin", "Matching outfits", "Cowboy hat", "Barefoot", "Jeans & Flip Flops", "Twerking", "Dancing", "Trip", "Double fisting", "Dumb hat", "Offensive shirt", "Couple fighting/Regular fight", "Pink taxi", "Ghost Tour/Street Train", "Sitting on the street", "Handshake", "High Five", "Litterbug", "Yawning", "Guy-on-Guy Scooter", "Hoofing it", "Spilled drink/food", "Acknowledge camera", "Almost get hit by car", "Crying", "American flag clothes", "Chug", "Can see FaceTime", "Go cart", "Tricycle", "Puking", "Drunk walk", "Skateboard", "Hug", "Luxury car", "Bachelorette party", "Aggressive honking", "Trying to get in wrong car", "Cop car/ambulance/fire truck", "Camera shy", "Bandage dude", "Formal attire", "Dropped something", "Nighttime shades", "Seperated at crosswalk", "Car delivery/pickup", "Car with ground effects"]
 sorted_occurrence = sorted(occurrence)
@@ -54,5 +54,3 @@ def generate():
 print("Get ready for Sloppy Joe's Bingo!")
 print("Get 5 across in any direction to win!")
 generate()
-
-# TODO: Transform into card format
